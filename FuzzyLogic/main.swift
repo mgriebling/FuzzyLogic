d9a1121c7e82d1525abfac6455af75d1c500c7a3
Michael Griebling <bob@c-inspirations.com>
2015-09-02 06:01:41 -0400

    Initial Commit

---
+++ FuzzyLogic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FuzzyLogic
+//
+//  Created by Mike Griebling on 2 Sep 2015.
+//  Copyright © 2015 Computer Inspirations. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
